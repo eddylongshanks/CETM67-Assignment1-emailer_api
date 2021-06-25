315c5ffe85c9b3874f2440e821c779f2f443366c
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2021-06-25 17:09:27 +0100

    added customised email address and name

--- emailer_api.py
+++ emailer_api.py
@@ -1,3 +1,4 @@
+import json
 import boto3
 from botocore.exceptions import ClientError
 
@@ -5,18 +6,38 @@ from botocore.exceptions import ClientError
 SENDER = "Newcastle Building Society <aws@holmescentral.co.uk>"
 AWS_REGION = "eu-west-2"
 SUBJECT = "Your Enquiry"
-BODY_TEXT = ("Thank you for your enquiry\r\n"
-             "A member of our team will call you within the next few days."
-            )
 CHARSET = "UTF-8"
 
 mailer = boto3.client('ses', region_name=AWS_REGION)
 
 def lambda_handler(event, context):
-    mail = send_it("chris@holmescentral.co.uk")
-
-def send_it(self, recipient):
     try:
+        print(str(event))
+        recipient_email = get_value(event, "email_address")
+        print(recipient_email)
+        
+        recipient_name = get_value(event, "first_name")
+        print(recipient_name)
+        
+        mailed_response = send_it(recipient_email, recipient_name)
+        print(str(mailed_response))
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps("Email sent! Message ID: " + mailed_response['MessageId'])
+        }
+        
+    except Exception as e:
+        print(str(e))
+        return {
+            'statusCode': 500,
+            'body': "1 " + json.dumps(str(e))
+        }
+
+def send_it(recipient, name):
+    try:
+        body_text = get_body(name)
+        
         response = mailer.send_email(
             Destination={
                 'ToAddresses': [
@@ -27,7 +48,7 @@ def send_it(self, recipient):
                 'Body': {
                     'Text': {
                         'Charset': CHARSET,
-                        'Data': BODY_TEXT,
+                        'Data': body_text,
                     },
                 },
                 'Subject': {
@@ -37,9 +58,28 @@ def send_it(self, recipient):
             },
             Source=SENDER        
         )
+        
+        return response
 
     except ClientError as e:
-        print(e.response['Error']['Message'])
-    else:
-        print("Email sent! Message ID:"),
-        print(response['MessageId'])
+        return {
+            'statusCode': 500,
+            'body': json.dumps(e.response['Error']['Message'])
+        }
+    
+def get_value(event, key):
+    try:
+        message = json.loads(event['Records'][0]['Sns']['Message'])
+        value = message[key]
+        return value
+        
+    except Exception as e:
+        raise e
+
+def get_body(name):
+    body = (f"Thank you {name} for your enquiry\r\n"
+         "A member of our team will call you within the next few days."
+        )
+    
+    return body
+    
